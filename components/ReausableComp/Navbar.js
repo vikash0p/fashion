0f00a7d3207ef979c6fd3cc8bcba0d5508941736
Vikash <119168528+vikash0p@users.noreply.github.com>
2024-05-09 19:07:24 +0530

    add the shadcn ui

--- components/ReausableComp/Navbar.js
+++ components/ReausableComp/Navbar.js
@@ -3,17 +3,25 @@ import Link from 'next/link'
 import React from 'react'
 import { IoCartSharp } from "react-icons/io5";
 import { useSelector } from 'react-redux';
-
+import { usePathname } from 'next/navigation'
 export default function Navbar() {
     const cart = useSelector((state) => state.allCart.cart);
+    const pathname = usePathname()
+
+    const links = [
+        { text: "Home", href: "/" },
+        { text: "Products", href: "/category" },
+        { text: "About", href: "/about" },
+        { text: "Contact Us", href: "/contact" }
+    ];
+
     return (
         <div className='flex justify-between w-full h-16 text-white bg-violet-800 items-center px-5 sticky top-0 left-0 right-0 z-50'>
-            <div className="text-2xl font-bold  font-mono">Fashion</div>
+            <div className="text-2xl font-bold font-mono">Fashion</div>
             <div className='space-x-4'>
-                <Link href={"/"} className="">Home</Link>
-                <Link href={"/category"} className="">Products</Link>
-                <Link href={"/about"} className="">About </Link>
-                <Link href={"/contact"} className="">Contact Us </Link>
+                {links.map((link, index) => (
+                    <Link key={index} href={link.href} className={`text-xl  ${link.href=== pathname ? "border-b ":""}`} >{link.text}</Link>
+                ))}
             </div>
             <Link href={"/cart"} className="flex"><IoCartSharp size={25} /> ({cart.length}) </Link>
         </div>
