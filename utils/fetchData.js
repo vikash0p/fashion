2f4f34b5a4785f113537b50e6c68dfabd4c9a759
Vikash <119168528+vikash0p@users.noreply.github.com>
2024-09-06 19:26:29 +0530

    change the logic in fetch Data

--- utils/fetchData.js
+++ utils/fetchData.js
@@ -1,13 +1,15 @@
 export const fetchData = async () => {
-    let data
     try {
         const res = await fetch("https://fakestoreapi.com/products");
-        if (!res.ok) throw new Error("Failed to fetch data");
-        data = await res.json();
-        return data
 
+        if (!res.ok) {
+            throw new Error(`Failed to fetch data: ${res.status} ${res.statusText}`);
+        }
+
+        const data = await res.json();
+        return data;
     } catch (error) {
-        console.log("🚀 ~ file: page.js:16 ~ error:", error);
-
+        console.error("Error fetching data line number 12:", error);
+        return null; // Or return an empty array [] if that's better for your use case
     }
-}
+};
