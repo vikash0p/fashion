ab654f0fa42d7b34314f7b088f8a790beec3641f
Vikash <119168528+vikash0p@users.noreply.github.com>
2024-09-07 16:32:25 +0530

    add the loading page

---
+++ app/loading.js
@@ -0,0 +1,12 @@
+'use client'
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div className='w-full min-h-screen flex flex-col gap-5 justify-center items-center'>
+          <div className="loader"></div>
+    </div>
+  )
+}
+
+export default Loading
