88a15ae13726100709eddc3276a941ab2f4c4cb2
Vikash <119168528+vikash0p@users.noreply.github.com>
2024-09-07 16:46:19 +0530

    add th data in sitemap function

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,22 +1,26 @@
 export default function sitemap() {
-    return [
-        {
-            url: 'https://acme.com',
-            lastModified: new Date(),
-            changeFrequency: 'yearly',
-            priority: 1,
-        },
-        {
-            url: 'https://acme.com/about',
-            lastModified: new Date(),
+    const currentDate = new Date().toISOString();
+
+    const urls = [
+        { url: 'https://storefashion.vercel.app/', changeFrequency: 'yearly', priority: 1 },
+        { url: 'https://storefashion.vercel.app/category', changeFrequency: 'monthly', priority: 0.8 },
+        { url: 'https://storefashion.vercel.app/about', changeFrequency: 'monthly', priority: 0.8 },
+        { url: 'https://storefashion.vercel.app/contact', changeFrequency: 'monthly', priority: 0.8 },
+        { url: 'https://storefashion.vercel.app/register', changeFrequency: 'monthly', priority: 0.8 },
+        { url: 'https://storefashion.vercel.app/login', changeFrequency: 'monthly', priority: 0.8 },
+        { url: 'https://storefashion.vercel.app/cart', changeFrequency: 'monthly', priority: 0.8 },
+        // Dynamically generating categories
+        ...Array.from({ length: 20 }, (_, i) => ({
+            url: `https://storefashion.vercel.app/category/${i + 1}`,
             changeFrequency: 'monthly',
             priority: 0.8,
-        },
-        {
-            url: 'https://acme.com/blog',
-            lastModified: new Date(),
-            changeFrequency: 'weekly',
-            priority: 0.5,
-        },
-    ]
+        })),
+    ];
+
+    return urls.map((urlData) => ({
+        url: urlData.url,
+        lastModified: currentDate,
+        changeFrequency: urlData.changeFrequency,
+        priority: urlData.priority,
+    }));
 }
